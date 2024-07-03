7597f06e67ba8b9bf9758e8e4089b9d358e5e004
keehwan <keehwan@weversecompany.com>
2024-07-03 23:11:35 +0900

    - [conf] swagger 의존성 추가 - [rename] infra.yml을 application-infra.yml 로 변경

---
+++ src/main/java/org/ccs/app/config/SwaggerConfig.java
@@ -0,0 +1,23 @@
+package org.ccs.app.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("ccs swagger docs").description("ccs rest api docs.").version("1.0.0");
+    }
+}
